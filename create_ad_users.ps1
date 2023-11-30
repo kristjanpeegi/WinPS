0789d142179f702bb06c642b14c86423f553eb0a
unknown <Administrator@SV-KOOL.LOCAL>
2023-11-30 01:21:22 -0800

    Täiendatud skripti nii, et ta kontrolliks kas kasutaja on juba olemas

--- create_ad_users.ps1
+++ create_ad_users.ps1
@@ -9,7 +9,16 @@ foreach ($user in $users){
     $username = Translit($username)
     $upname = $username + "@sv-kool.local"
     $displayname = $user.FirstName + " " + $user.LastName
-    New-ADUser -Name $username -DisplayName $displayname -GivenName $user.FirstName -Surname $user.LastName -Department $user.Department -Title $user.Role -UserPrincipalName $upname -AccountPassword (ConvertTo-SecureString $user.Password -AsPlainText -force) -Enabled $true
+
+    #Kontrollime kas kasutaja on olemas või ei. Kui kasutaja on olemas tuleb viisakas veateade.
+    $existingUser = Get-ADUser -Filter {SamAccountName -eq $username}
+    if ($existingUser -eq $null) {
+        New-ADUser -Name $username -DisplayName $displayname -GivenName $user.FirstName -Surname $user.LastName -Department $user.Department -Title $user.Role -UserPrincipalName $upname -AccountPassword (ConvertTo-SecureString $user.Password -AsPlainText -force) -Enabled $true
+    $LASTEXITCODE = $?
+    } else {
+        Write-Host "Kasutaja $username on juba olemas!"
+        $LASTEXITCODE = 1
+    }
 }
 #Lisame funktsiooni mis muudab UTF-8 sümbolid LATIN charsetile sobivaks
 function Translit {
